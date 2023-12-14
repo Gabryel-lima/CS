2e8e1a4d56b026b2e35ee976d9fa27bfde1cb547
Gabryel-lima <gabbryellimasi@gmail.com>
2023-12-14 17:55:05 -0300

    .

--- Curso1/Program.cs
+++ Curso1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void HelloWorld() {
+    Console.WriteLine("Hello, World!");
+}
+
+void Main() {
+    HelloWorld();
+}
+
+Main();
--- Curso1/obj/Debug/net8.0/Curso1.AssemblyInfo.cs
+++ Curso1/obj/Debug/net8.0/Curso1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Curso1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74bc9bc7b5bae49d7091be65d65e27f27341a3af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Curso1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Curso1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
